1035e43f7dec9c5e8c4eb16984c6cc0476257810
rhartig-ct <rhartig@ciphertechsolutions.com>
2026-02-11 10:02:07 -0700

    Convert cli to use click, added list command

--- src/yara_registry/cli.py
+++ src/yara_registry/cli.py
@@ -1,23 +1,46 @@
-import argparse
 from pathlib import Path
 import shutil
+
+import click
 import platformdirs
-import logging
 
-logger = logging.getLogger(__name__)
+import yara_registry
 
-parser = argparse.ArgumentParser()
-parser.add_argument("namespace", type=str, help="Scope of rules, this should be considered equivalent to python package name, so ensure it does not conflict ('-', and '_' are considered interchangeable in package naming.)")
-parser.add_argument("path", type=str, help="Path to directory containing rules, files must end in '.yar' or '.yara'")
-
+@click.group(context_settings={"help_option_names": ["-h", "--help"]})
 def main():
-    args = parser.parse_args()
-    namespace = args.namespace
-    path = Path(args.path)
+    pass
+
+@main.command(
+    help="""Add new yara files not contained in a python package"""
+)
+@click.option(
+    "--namespace",
+    required=True,
+    type=str,
+    help="Scope of rules, this should be considered equivalent to python package name, so ensure it does not conflict ('-', and '_' are considered interchangeable in package naming.)",
+)
+@click.option(
+    "--path",
+    required=True,
+    type=click.Path(
+        exists=True,
+        dir_okay=True,
+        file_okay=True,
+        readable=True,
+        path_type=Path,
+    ),  
+    help="Path to directory containing rules, files must end in '.yar' or '.yara'",
+)
+def add(namespace: str, path: Path):
     data_dir = Path(platformdirs.user_data_dir("yara_registry", appauthor=False))
-    logger.info(f"Installing rules to {data_dir}")
+    click.echo(f"Installing rules to {data_dir}")
     rules_namespace_path = data_dir / "rules" / namespace
     shutil.copytree(path, rules_namespace_path, dirs_exist_ok=True)
 
-if __name__ == "__main__":
-    main()
+@main.command(
+    help="""Add new yara files not contained in a python package""",
+    name="list"
+)
+def _list():
+    for source in yara_registry.corpus.items():
+        click.echo(source)
